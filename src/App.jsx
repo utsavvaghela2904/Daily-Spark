97ee975f63368f47b1926cdd6638246ced67e9d1
utsavvaghela2904 <utsavvaghela2904@gmail.com>
2024-08-09 18:34:43 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,55 @@ import React, { Component } from 'react'
 import NavBar from './components/NavBar'
 import News from './components/News'
 import './App.css';
+import PropTypes from 'prop-types'
+import Aboutus from './components/Aboutus';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-export class App extends Component {z
+export class App extends Component {
+    static defaultProps = {
+        pageSize: 6,
+        country: "us",
+        category: "technology"
+    }
+
+    static propTypes = {
+        pageSize: PropTypes.number,
+        country: PropTypes.string,
+        category: PropTypes.string,
+    }
+
+    constructor(props){
+        super(props);
+            this.state = {
+                isDarkMode: false,
+            }
+        }
+    toggleMode = () => {
+        this.setState(prevState => ({
+            isDarkMode :!prevState.isDarkMode
+        }))
+    }
+
     render() {
+        const { isDarkMode } = this.state;
+        const themeClass = isDarkMode ? 'dark-mode' : 'light-mode';
         return (
             <>
-                <NavBar />
-                <News pageSize={6} />
+                <Router>
+                    <div className={`app ${themeClass}`}>
+                    <NavBar toggleMode={this.toggleMode}/>
+                    <Routes>
+                        <Route path='/' element={<News key="general" pageSize={6} country={"in"} category={"general"} />} />
+                        <Route path='/business' element={<News key="business" pageSize={6} country={"in"} category={"business"} />} />
+                        <Route path='/entertainment' element={<News key="entertainment" pageSize={6} country={"in"} category={"entertainment"} />} />
+                        <Route path='/health' element={<News key="health" pageSize={6} country={"in"} category={"health"} />} />
+                        <Route path='/science' element={<News key="science" pageSize={6} country={"in"} category={"science"} />} />
+                        <Route path='/sports' element={<News key="sports" pageSize={6} country={"in"} category={"sports"} />} />
+                        <Route path='/technology' element={<News key="technology" pageSize={6} country={"in"} category={"technology"} />} />
+                        <Route path='/Aboutus' element={<Aboutus/>}></Route>
+                    </Routes>
+                    </div>
+                </Router>
             </>
         )
     }
